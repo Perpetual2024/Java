4c78821996b882097d9edd89c3f96bc56d315800
Perpetual Akinyi <you@example.com>
2025-02-28 16:01:10 +0300

    learned the syntax of Java

---
+++ Index.java
@@ -0,0 +1,14 @@
+public class Index {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+        if (args.length > 20) {
+            System.out.println("Too many arguments!");
+        } else {
+            System.out.println("Argument is okay");
+        }
+    }
+}
+
+g
