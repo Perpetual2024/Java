ddb462ae2e1db3c90f5bbd9e2add7fd05135f020
Perpetual Akinyi <you@example.com>
2025-04-01 15:48:59 +0300

    initialized arrays

--- Index.java
+++ Index.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class Index {
     public static void main(String[] args) {
-        
-        
-            byte age = 30
-            System.out.println(name);
+        int [] arrays = {1, 3, 5, 7, 2, 4, 6, 8};
+        Arrays.sort(arrays);
+        System.out.println(Arrays.toString(arrays));
     }
-}
+    }
+
 
