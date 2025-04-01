e1884d7bc92bcbeccfe100e2640fdadb98dfe794
Perpetual Akinyi <you@example.com>
2025-04-01 16:01:26 +0300

    arithmetic expressions

--- Index.java
+++ Index.java
@@ -5,7 +5,16 @@ public class Index {
         int [] arrays = {1, 3, 5, 7, 2, 4, 6, 8};
         Arrays.sort(arrays);
         System.out.println(Arrays.toString(arrays));
+        //multi dimensional arrays
+        int [] [] numbers = new int [1] [2];
+
+        numbers[0][0] = 1;
+
+        //arithmetic expressions
+        int x = 1;
+         x -= 2; //reuces x by 2 augmentes sidns
     }
+
     }
 
 
