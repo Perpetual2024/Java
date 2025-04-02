55675ea3e6e67616932e44929773feb5841f2894
Perpetual Akinyi <you@example.com>
2025-04-02 13:27:55 +0300

    Declaring variables

--- Index.java
+++ Index.java
@@ -1,20 +1,17 @@
-import java.util.Arrays;
-
 public class Index {
     public static void main(String[] args) {
-        int [] arrays = {1, 3, 5, 7, 2, 4, 6, 8};
-        Arrays.sort(arrays);
-        System.out.println(Arrays.toString(arrays));
-        //multi dimensional arrays
-        int [] [] numbers = new int [1] [2];
+        int age = 36;
+        double price = 99.99;
+        char grade = 'B';
+        boolean isAdmin = true;
+        String name = "Perpetual";
 
-        numbers[0][0] = 1;
-
-        //arithmetic expressions
-        int x = 1;
-         x -= 2; //reuces x by 2 augmentes sidns
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Admin: " + isAdmin);
     }
-
-    }
+}
 
 
