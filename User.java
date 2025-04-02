f2e90f04e317066db13421a8a090a648e8379491
Perpetual Akinyi <you@example.com>
2025-04-02 13:33:14 +0300

    Getting user input from Scanner

---
+++ User.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;// import scanner
+
+public class User {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);// create scanner object
+
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+    }
+
+
+    
+}
