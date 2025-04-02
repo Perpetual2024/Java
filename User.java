50ffb70f5fd4a89931350b45fede8875c63e9b71
Perpetual Akinyi <you@example.com>
2025-04-02 13:38:22 +0300

    Finished scanner input

--- User.java
+++ User.java
@@ -6,7 +6,13 @@ public class User {
         Scanner scanner = new Scanner(System.in);// create scanner object
 
         System.out.println("Enter your name:");
-        String name = scanner.nextLine();
+        String name = scanner.nextLine();// Read a String input
+
+        System.out.println("Enter your age; ");
+        int age = scanner.nextInt(); // read an int
+
+        System.out.println("Hello, " + name + "! you are " + age + " years old.");
+        scanner.close();
     }
 
 
