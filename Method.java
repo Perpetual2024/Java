c5afbec2132c059a368c02bd31fe63f97056388e
Perpetual Akinyi <you@example.com>
2025-04-03 16:38:58 +0300

    completed arrays

---
+++ Method.java
@@ -0,0 +1,33 @@
+//public class Methods {
+   // public static void greet(String name) {
+     //   System.out.println("Hello, World!" + name);
+   // }
+        // ✅ Avoid repetition
+        // ✅ Make code modular and reusable
+        // ✅ Improve readability
+        // A method is written outside main() but is called inside main().eg
+        
+        
+    
+    //methods with return values
+    /*public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int sum = add(5, 7);
+        System.out.println("Sum: " + sum);
+    }*/
+   public class Method{
+    public static int mul(int a , int b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        int result = mul(5, 7);
+        System.out.println("Result: " + result);
+    }
+ }
+
+
+    //The above is a  defination ofa method
+
