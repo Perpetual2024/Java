9bc273aa34b83287aa35e0ed199d91b6bd45d355
Perpetual Akinyi <you@example.com>
2025-04-03 14:47:29 +0300

    leaned nested arrays matrixes ad loops on them

--- Arrays.java
+++ Arrays.java
@@ -9,5 +9,32 @@ public class Arrays {
 
         numbers[2]=98;
         System.out.println("First element: " + numbers[2]);
+
+        //Looping through arrays
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + " is " + numbers[i]);
+        }
+        //simpler loop
+        for(int num : numbers){
+            System.out.println("Number: " + num);
+        }
+
+        //Multi-Dimensional Arrays
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println("Element at (1,1): " + matrix[1][1]);
+
+        //nested loops
+        for(int i = 0; i < matrix.length; i++){ // Loop through rows
+            for(int j = 0; j < matrix[i].length; j++){ // Loop through columns
+                System.out.print(matrix[i][j] + " ");
+
+            }
+            System.out.println(); // New line for each row
+        }
+           
     }
 }
